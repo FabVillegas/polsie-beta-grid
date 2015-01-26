c7532e5fa242284c485ae44ec1b83bb776490d8b
Fabritzio Villegas <f.villegas93@gmail.com>
2015-01-25 21:09:49 -0600

    almost ready

--- app/js/controllers/secondCtrl.js
+++ app/js/controllers/secondCtrl.js
@@ -1,9 +1,19 @@
 angular.module('polsie-beta-grid').controller('secondCtrl', secondCtrl);
 
-secondCtrl.$inject = ['$scope', '$state'];
+secondCtrl.$inject = ['$scope', '$state', '$document'];
 
-function secondCtrl($scope, $state){
-  $scope.changeState = function(){
-    $state.go('first');
+function secondCtrl($scope, $state, $document){
+
+
+  if($document[0].body.offsetWidth < 768){
+    $scope.isShown = false;
+  }
+  else{
+    $scope.isShown = true;
+  }
+
+  $scope.showLinks = function(){
+    $scope.isShown = !$scope.isShown;
   };
+
 };
