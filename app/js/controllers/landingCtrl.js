e81b0f5253663660b55d8b2b123a69ddd3d68ee3
FabVillegas <f.villegas93@gmail.com>
2015-01-29 11:35:54 -0600

    Fixed icon

--- app/js/controllers/landingCtrl.js
+++ app/js/controllers/landingCtrl.js
@@ -4,15 +4,6 @@ landingCtrl.$inject = ['$scope', '$state', '$document'];
 
 function landingCtrl($scope, $state, $document){
 
-  $scope.displayIcon = true;
-
-  if($document[0].body.offsetWidth < 768){
-    $scope.isShown = false;
-  }
-  else{
-    $scope.isShown = true;
-  }
-
   $scope.showLinks = function(){
     $scope.isShown = !$scope.isShown;
   };
--- app/js/directives/toggleMenu.js
+++ app/js/directives/toggleMenu.js
@@ -3,10 +3,10 @@ angular.module('polsie-beta-grid').directive('toggleMenu', function($window, $do
     restrict: 'C', // detects an classname
     scope: false, // parent scope
     link: function(scope, element, attrs){
-
       if ($document[0].body.offsetWidth >= 768){
         scope.displayIcon = false;
         scope.isShown = true;
+        scope.$apply('isShown = ' + true); /* show menu */
       }
       else{
         scope.displayIcon = true;
@@ -16,12 +16,13 @@ angular.module('polsie-beta-grid').directive('toggleMenu', function($window, $do
       scope.display = function(value){
         scope.$apply('displayIcon = ' + value);
       };
+
       target.bind('resize', function(){
         var currentSize = $document[0].body.offsetWidth;
         if (currentSize < 768){
           scope.display(true);
         }
-        else{
+        else if(currentSize >= 768 ){
           scope.display(false);
           scope.$apply('isShown = ' + true); /* show menu */
         }
