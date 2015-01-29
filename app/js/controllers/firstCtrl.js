391951c66ea21fdc2d6dd5b262ceafb607fcf16b
FabVillegas <f.villegas93@gmail.com>
2015-01-29 10:32:29 -0600

    Fixed content

--- app/js/controllers/firstCtrl.js
+++ app/js/controllers/firstCtrl.js
@@ -1,5 +1,13 @@
 angular.module('polsie-beta-grid').controller('firstCtrl', firstCtrl);
 
+<<<<<<< HEAD
+firstCtrl.$inject = ['$scope', '$state'];
+
+function firstCtrl($scope, $state){
+  $scope.changeState = function(){
+    $state.go('second');
+  };
+=======
 firstCtrl.$inject = ['$scope', '$state', '$document'];
 
 function firstCtrl($scope, $state, $document){
@@ -15,4 +23,5 @@ function firstCtrl($scope, $state, $document){
     $scope.isShown = !$scope.isShown;
   };
 
+>>>>>>> master
 };
